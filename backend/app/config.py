ca8e0b549455c820ab89d7694144b688766f0031
Mspawan <128341657+Mspawan@users.noreply.github.com>
2025-11-12 13:34:48 +0530

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,26 +1,18 @@
-# from pydantic_settings import BaseSettings
-
-# class Settings(BaseSettings):
-#     database_url: str
-#     backend_host: str = "0.0.0.0"
-#     backend_port: int = 8000
-#     allowed_origins: list = ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3001"]
-
-#     class Config:
-#         env_file = ".env"
-#         env_file_encoding = "utf-8"
-#         extra = "ignore"  # prevents errors if extra env vars exist
-
-# settings = Settings()
-
+import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     # PostgreSQL database connection
-    # Format: postgresql+asyncpg://username:password@host:port/database_name
-    database_url: str = "postgresql+asyncpg://pawanadmin:Azure%4012345@ticketingdbserver.postgres.database.azure.com:5432/postgres"
+    # It will first check for DATABASE_URL in environment variables (Azure),
+    # and if not found, use the fallback value for local testing.
+    database_url: str = os.getenv(
+        "DATABASE_URL",
+        "postgresql+asyncpg://pawanadmin:Azure%4012345@ticketingdbserver.postgres.database.azure.com:5432/postgres"
+    )
+    
     backend_host: str = "0.0.0.0"
     backend_port: int = 8000
+    allowed_origins: list = ["http://localhost:3000", "http://localhost:3001", "http://127.0.0.1:3001"]
 
     class Config:
         env_file = ".env"
